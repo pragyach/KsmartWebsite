cb908609def9bd200d1b8fafeda5c543d26f5ac2
kaiki26 <kaiki@uixlabs.in>
2024-12-08 11:49:14 +0530

    adding description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,6 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-   <><div>
-      <Head>
-        <title>KredSmart.ai</title>
-        <meta property="og:title" content="My page title" key="title" />
-      </Head>
-    </div><MyComponent></MyComponent></>
+   <MyComponent></MyComponent>
   );
 }
