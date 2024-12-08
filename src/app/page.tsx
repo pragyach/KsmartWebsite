b3a5a1a307da1138ad9f9d7a2b0a455feeade920
kaiki26 <kaiki@uixlabs.in>
2024-12-08 11:44:08 +0530

    Adding meta title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import MyComponent from "../components/y/MyComponent"
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <MyComponent></MyComponent>
+   <><div>
+      <Head>
+        <title>KredSmart.ai</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+    </div><MyComponent></MyComponent></>
   );
 }
